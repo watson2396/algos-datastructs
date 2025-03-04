e6ac21fab78b9f7da9fbe785012cc92792b90839
Jacob Watson <JacobWatson@sbp.com>
2025-03-04 16:02:55 -0500

    feat: started sets

--- datastructs/sets/sets.go
+++ datastructs/sets/sets.go
@@ -1,5 +1,27 @@
 package sets
 
+import (
+	"github.com/watson2396/algos-datastructs/datastructs/linkedlists"
+)
+
+type Set struct{ linkedlists.DList }
+
 func SetInit() {
 
 }
+
+// Set insert
+
+// Set remove
+
+// Set union
+
+// Set intersection
+
+// Set difference
+
+// Set is member
+
+// Set is subset
+
+// Set is equal
